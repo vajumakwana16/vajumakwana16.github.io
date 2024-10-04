320bbcede71936e910e7ab61aebdb993c52da431
Vaju Makwana <93653144+vajumakwana16@users.noreply.github.com>
2024-10-04 09:40:27 +0530

    Added About info

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"d81edf635a9f47fd8b8dd1dc94afdb3eaffe51
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4087938098"
+    serviceWorkerVersion: "165346905"
   }
 });
